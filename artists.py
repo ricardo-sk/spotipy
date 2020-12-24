4b29f841e48ddf91da45993da0a8767eac592e8f
ricardo-sk <76498129+ricardo-sk@users.noreply.github.com>
2020-12-24 15:10:17 -0300

    Add files via upload

---
+++ artists.py
@@ -0,0 +1,244 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018-2019 Streamlit Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""An example of showing geographic data."""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import spotipy
+
+
+# SETTING PAGE CONFIG TO WIDE MODE
+st.set_page_config(layout="wide",  initial_sidebar_state="expanded", page_title="Spotify",page_icon="🧊",)
+
+
+
+
+import spotipy.util as util
+
+username = 'ricardo.sk'#st.text_input(label='username')
+client_id ='c74692207a81490aad99dbb253113362'
+
+client_secret = '76f566d072a14f2385562de2069fcd50'
+redirect_uri = 'http://localhost:7777/callback'
+scope = 'user-top-read'
+
+token = util.prompt_for_user_token(username=username, 
+                                   scope=scope, 
+                                   client_id=client_id,   
+                                   client_secret=client_secret,     
+                                   redirect_uri=redirect_uri)
+
+
+
+sp = spotipy.Spotify(auth=token)
+
+def analyze_playlist(creator, playlist_id):
+    
+    # Create empty dataframe
+    playlist_features_list = ["artist","album","track_name",  "track_id","danceability","energy","key","loudness","mode", "speechiness","instrumentalness","liveness","valence","tempo", "duration_ms","time_signature"]
+    
+    playlist_df = pd.DataFrame(columns = playlist_features_list)
+    
+    # Loop through every track in the playlist, extract features and append the features to the playlist df
+    
+    playlist = sp.user_playlist_tracks(creator, playlist_id)["items"]
+    for track in playlist:        # Create empty dict
+        playlist_features = {}        # Get metadata
+        playlist_features["artist"] = track["track"]["album"]["artists"][0]["name"]
+        playlist_features["artistid"] = track["track"]["album"]["artists"][0]["id"]
+        playlist_features["album"] = track["track"]["album"]["name"]
+        playlist_features["track_name"] = track["track"]["name"]
+        playlist_features["track_id"] = track["track"]["id"]
+        
+        # Get audio features
+        audio_features = sp.audio_features(playlist_features["track_id"])[0]
+        for feature in playlist_features_list[4:]:
+            playlist_features[feature] = audio_features[feature]
+        
+        # Concat the dfs
+        track_df = pd.DataFrame(playlist_features, index = [0])
+        playlist_df = pd.concat([playlist_df, track_df], ignore_index = True)
+        
+    return playlist_df
+
+# LOADING DATA
+
+
+
+
+
+
+# CREATING FUNCTION FOR MAPS
+
+artist=st.text_input(label='artist', value='')
+result = sp.search(artist, type='artist')
+
+artist_uri = result['artists']['items'][0]['uri']
+
+art1=sp.artist(artist_id=artist_uri)
+name=art1['name']
+foto=art1['images'][1]['url']
+st.title(name)
+st.image(foto)
+genres=art1['genres']
+st.text(genres)
+
+sp_albums = sp.artist_albums(artist_uri, album_type='album')
+#Store artist's albums' names' and uris in separate lists
+album_names = []
+album_uris = []
+for i in range(len(sp_albums['items'])):
+    album_names.append(sp_albums['items'][i]['name'])
+    album_uris.append(sp_albums['items'][i]['uri'])
+
+
+
+albumselec=st.multiselect(options=album_names, label='Albums')
+
+def albumSongs(uri):
+    album = uri #assign album uri to a_name
+    spotify_albums[album] = {} #Creates dictionary for that specific album
+    #Create keys-values of empty lists inside nested dictionary for album
+    spotify_albums[album]['album'] = [] #create empty list
+    spotify_albums[album]['track_number'] = []
+    spotify_albums[album]['id'] = []
+    spotify_albums[album]['name'] = []
+    spotify_albums[album]['uri'] = []
+    tracks = sp.album_tracks(album) #pull data on album tracks
+    for n in range(len(tracks['items'])): #for each song track
+            spotify_albums[album]['album'].append(album_names[album_count]) #append album name tracked via album_count
+            spotify_albums[album]['track_number'].append(tracks['items'][n]['track_number'])
+            spotify_albums[album]['id'].append(tracks['items'][n]['id'])
+            spotify_albums[album]['name'].append(tracks['items'][n]['name'])
+            spotify_albums[album]['uri'].append(tracks['items'][n]['uri'])
+
+            
+            
+def audio_features(album):
+    #Add new key-values to store audio features
+    spotify_albums[album]['acousticness'] = []
+    spotify_albums[album]['danceability'] = []
+    spotify_albums[album]['energy'] = []
+    spotify_albums[album]['instrumentalness'] = []
+    spotify_albums[album]['liveness'] = []
+    spotify_albums[album]['loudness'] = []
+    spotify_albums[album]['speechiness'] = []
+    spotify_albums[album]['tempo'] = []
+    spotify_albums[album]['valence'] = []
+    spotify_albums[album]['popularity'] = []
+    #create a track counter
+    track_count = 0
+    for track in spotify_albums[album]['uri']:
+        #pull audio features per track
+        features = sp.audio_features(track)
+        
+        #Append to relevant key-value
+        spotify_albums[album]['acousticness'].append(features[0]['acousticness'])
+        spotify_albums[album]['danceability'].append(features[0]['danceability'])
+        spotify_albums[album]['energy'].append(features[0]['energy'])
+        spotify_albums[album]['instrumentalness'].append(features[0]['instrumentalness'])
+        spotify_albums[album]['liveness'].append(features[0]['liveness'])
+        spotify_albums[album]['loudness'].append(features[0]['loudness'])
+        spotify_albums[album]['speechiness'].append(features[0]['speechiness'])
+        spotify_albums[album]['tempo'].append(features[0]['tempo'])
+        spotify_albums[album]['valence'].append(features[0]['valence'])
+        #popularity is stored elsewhere
+        pop = sp.track(track)
+        spotify_albums[album]['popularity'].append(pop['popularity'])
+        track_count+=1
+        
+        
+
+spotify_albums = {}
+album_count = 0
+for i in album_uris: #each album
+    albumSongs(i)
+    print("Album " + str(album_names[album_count]) + " songs has been added to spotify_albums dictionary")
+    album_count+=1 #Updates album count once all tracks have been added
+
+    
+import time
+import numpy as np
+sleep_min = 2
+sleep_max = 5
+start_time = time.time()
+request_count = 0
+for i in spotify_albums:
+    audio_features(i)
+    request_count+=1
+    if request_count % 5 == 0:
+        print(str(request_count) + " playlists completed")
+        time.sleep(np.random.uniform(sleep_min, sleep_max))
+        print('Loop #: {}'.format(request_count))
+        print('Elapsed Time: {} seconds'.format(time.time() - start_time))
+        
+        
+        
+        
+dic_df = {}
+dic_df['album'] = []
+dic_df['track_number'] = []
+dic_df['id'] = []
+dic_df['name'] = []
+dic_df['uri'] = []
+dic_df['acousticness'] = []
+dic_df['danceability'] = []
+dic_df['energy'] = []
+dic_df['instrumentalness'] = []
+dic_df['liveness'] = []
+dic_df['loudness'] = []
+dic_df['speechiness'] = []
+dic_df['tempo'] = []
+dic_df['valence'] = []
+dic_df['popularity'] = []
+for album in spotify_albums: 
+    for feature in spotify_albums[album]:
+        dic_df[feature].extend(spotify_albums[album][feature])
+        
+len(dic_df['album'])
+
+df = pd.DataFrame.from_dict(dic_df)
+df['artist'] = name        
+
+import plotly.express as px
+
+scater=px.scatter(df,'valence','energy' ,range_y=[0,1], range_x=[0,1],width=1000,
+          color='album', 
+          height=600,
+          hover_name='name',
+          hover_data=['name'],
+          title=name,
+         
+         
+         
+          color_discrete_sequence=px.colors.qualitative.Set1, template='plotly_dark')
+
+scater.update_layout(shapes=[
+    dict(
+      type= 'line',
+      yref= 'paper', y0= 0, y1= 1,
+      xref= 'x', x0= 0.5, x1= 0.5
+    ),
+       dict(
+      type= 'line',
+      yref= 'paper', y0= 0.5, y1= 0.5,
+      xref= 'x', x0= 0, x1= 1
+    )
+])
+
+
+st.plotly_chart(scater)
